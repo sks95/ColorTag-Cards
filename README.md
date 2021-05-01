4a8f97dd0d9754b3b8247db7dfda3a50b53172c4
SHIVAM <yyyshivamyyy@gmail.com>
2021-05-01 12:10:26 +0530

    :zap: adding features and new functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+## ColorTag Cards
+
+Create Todo cards based on color-tagging with basic UI
--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 let colorBtn = document.querySelectorAll(".filter_color");
 let mainContainer = document.querySelector(".main_container");
 let body = document.body;
+let crossBtn = document.querySelector(".fa-times");
 let plusButton = document.querySelector(".fa-plus");
-
-for(let i = 0; i < colorBtn.length; i++){
-    colorBtn[i].addEventListener("click", function(e){
-        let color = colorBtn[i].classList[1];
-        mainContainer.style.backgroundColor = color;
-    })
-}
-{/* <div class="modal_container">
-            <div class="input_container">
-                <textarea class="modal_input" placeholder="Enter your text here"></textarea>
-            </div>
-            <div class="modal_filter_container">
-                <div class="filter pink"></div>
-                <div class="filter blue"></div>
-                <div class="filter green"></div>
-                <div class="filter black"></div>
-            </div>
-        </div> */}
+let deleteState = false;
 
 plusButton.addEventListener("click", createModal);
-function createModal(){
-    // create modal
-    let modal_container = document.createElement("div");
-    modal_container.setAttribute("class","modal_container");
-    modal_container.innerHTML = `<div class="input_container">
-    <textarea class="modal_input" placeholder="Enter your text here"></textarea>
-</div>
-<div class="modal_filter_container">
-    <div class="filter pink"></div>
-    <div class="filter blue"></div>
-    <div class="filter green"></div>
-    <div class="filter black"></div>
-</div>`;
+crossBtn.addEventListener("click", setDeleteState);
 
-    body.appendChild(modal_container);
-    // handle event listener part
-    handleModal(modal_container);
+function createModal(){
+        // create modal
+        let modalContainer = document.querySelector(".modal_container");
+        if(modalContainer == null){
+            let modalContainer = document.createElement("div");
+            modalContainer.setAttribute("class","modal_container");
+            modalContainer.innerHTML = `<div class="input_container">
+        <textarea class="modal_input" placeholder="Enter your text here"></textarea>
+    </div>
+    <div class="modal_filter_container">
+        <div class="filter pink"></div>
+        <div class="filter blue"></div>
+        <div class="filter green"></div>
+        <div class="filter black"></div>
+    </div>`;
+
+        body.appendChild(modalContainer);
+        // handle event listener part
+        handleModal(modalContainer);
+    }
+    // clicking plus button again -> reset text area
+    let textarea = modalContainer.querySelector(".modal_input");
+    textarea.value = "";
 }
 function handleModal(modal_container){
     let cColor = "black";
@@ -70,11 +62,46 @@ function handleModal(modal_container){
 }
 function createTask(color, task){
     let taskContainer = document.createElement("div");
+    
+    let uid = new ShortUniqueId();
+
     taskContainer.setAttribute("class","task_container");
     taskContainer.innerHTML = `<div class="task_filter ${color}"></div>
     <div class="task_desc_container">
-        <h3 class="uid">#example</h3>
-    <div class="task_desc">${task}</div>
+        <h3 class="uid">#${uid()}</h3>
+    <div class="task_desc" contenteditable="true" >${task}</div>
 </div>`
     mainContainer.appendChild(taskContainer);
+    let taskFilter = taskContainer.querySelector(".task_filter");
+    taskFilter.addEventListener("click", changeColor);
+    taskContainer.addEventListener("click",deleteTask);
+}
+function changeColor (e){
+    taskFilter = e.currentTarget;
+    let colors = ["pink", "blue", "green", "black"];
+    let cColor = taskFilter.classList[1];
+    let idx = colors.indexOf(cColor);
+    let newColorIdx = (idx + 1) % 4;
+    taskFilter.classList.remove(cColor);
+    taskFilter.classList.add(colors[newColorIdx]);
+
+}
+function setDeleteState(e){
+    // function to add color toggle over cross button
+    let crossBtn = e.currentTarget;
+    let parent = crossBtn.parentNode;
+    
+    if(deleteState == false){
+        parent.classList.add("active");
+    }else{
+        parent.classList.remove("active");
+    }
+    deleteState = !deleteState; 
+}
+function deleteTask(e){
+    let taskContainer = e.currentTarget;
+    if(deleteState){
+        taskContainer.remove();
+    }
+
 }
